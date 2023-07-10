b7dc15c99a31037d90d1c7c746d8c81be15fd972
Mkeshk91 <maikeshk@hotmail.com>
2023-07-10 07:45:40 -0700

    a

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
 *
 * Return: Always 0 (Sucess)
 */
-int main(int argc, char **argv[])
+int main(int argc, char const *argv[])
 {
 	(void)argc;
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
 }
