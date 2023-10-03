71516155709c28ceb9b77a14633a43d0da52363d
Mkeshk91 <maikeshk@hotmail.com>
2023-10-03 10:42:00 -0700

    m1

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - writes  character c t stdout.
+ * @c: character t print
+ * Return: On success 1
+ * On error  -1 is returned
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
