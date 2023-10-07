fa1945cd33229f4623dfaebdd4d415825f585a03
Mkeshk91 <maikeshk@hotmail.com>
2023-10-07 11:22:01 -0700

    m3

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,7 +7,7 @@
 
 int _putchar(char c);
 
-void *malloc_checked(unsigned int b);
+void *malloc_checked(unsigned int a);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
